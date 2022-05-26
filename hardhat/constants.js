804c2f1f14918858d28cc932cbfdd9b2c163272b
Kemal Cleva <68566006+KemalOnWeb3@users.noreply.github.com>
2022-05-26 23:21:41 +0200

    y

---
+++ hardhat/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x0353eF3ADc48559d8dDb782599bb614B93E24f56";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
